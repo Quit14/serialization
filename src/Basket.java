e71df210d8f32904b49058ec0bbb343c25d1fcee
Anastasiya Volkova <ahh.write@gmail.com>
2022-09-16 19:31:11 +0300

    Запись и воссоздание корзины из текстового файла

--- src/Basket.java
+++ src/Basket.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,6 +17,12 @@ public class Basket {
         this.amount = new int[names.length];
     }
 
+    public Basket(String[] names, double[] prices, int[] amount) {
+        this.names = names;
+        this.prices = prices;
+        this.amount = amount;
+    }
+
     public void addToCart(int productNum, int amount) {
         this.amount[productNum - 1] += amount;
     }
@@ -21,6 +30,41 @@ public class Basket {
     public int[] getAmount() {
         return amount;
     }
+// запись корзины в файл
+    protected void saveTxt(File file) {
+        try (PrintWriter saveTxt = new PrintWriter(file)) {
+            for (int i = 0; i < names.length; i++) {
+                saveTxt.print(names[i] + " " + prices[i]+ " " + amount[i] + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//воссоздание корзины из файла
+    public static Basket loadFromTxtFile(File textFile) {
+        try {
+            BufferedReader buff = new BufferedReader(new FileReader(textFile));
+            String s;
+            List<String> names = new ArrayList<>();
+            List<Double> prices = new ArrayList<>();
+            List<Integer> amount = new ArrayList<>();
+            while ((s = buff.readLine()) != null) {
+                String[] parts = s.split(" ");
+                names.add(parts[0]);
+                prices.add(Double.parseDouble(parts[1]));
+                amount.add(Integer.parseInt(parts[2]));
+            }
+            String[] name = names.toArray(new String[names.size()]);
+            double[] price = prices.stream().mapToDouble(d -> d).toArray();
+            int[] amounts = amount.stream().mapToInt(i -> i).toArray();
+            buff.close();
+
+            return new Basket(name, price, amounts);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public void printCart() {
         IntStream
